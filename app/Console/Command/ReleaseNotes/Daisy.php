3a6a9663045d1b04c42dbb52dcb89fa9a14d5b2e
crlcu <adrian.crisan88@gmail.com>
2016-11-09 15:29:00 +0000

    daisy:release-notes log between start/end date

--- app/Console/Command/ReleaseNotes/Daisy.php
+++ app/Console/Command/ReleaseNotes/Daisy.php
@@ -11,6 +11,8 @@ use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 
+use Carbon\Carbon;
+
 use Twig_Loader_Filesystem;
 use Twig_Environment;
 
@@ -20,6 +22,7 @@ use Swift_Mailer;
 
 class Daisy extends Command
 {
+    private $input;
     private $command;
 
     protected function configure()
@@ -29,6 +32,8 @@ class Daisy extends Command
             ->setDefinition(
                 new InputDefinition([
                     new InputOption('notify', null, InputOption::VALUE_OPTIONAL, 'List of emails separated by comma'),
+                    new InputOption('send-email', null, InputOption::VALUE_NONE, 'Send email.'),
+                    new InputOption('html', null, InputOption::VALUE_NONE, 'Output as html.'),
                     new InputOption('username', null, InputOption::VALUE_OPTIONAL, 'SVN Username'),
                     new InputOption('password', null, InputOption::VALUE_OPTIONAL, 'SVN Password'),
                     new InputOption('self-service', null, InputOption::VALUE_REQUIRED, 'SelfService start,end revisions'),
@@ -36,19 +41,25 @@ class Daisy extends Command
                     new InputOption('dwp', null, InputOption::VALUE_REQUIRED, 'DWP start,end revisions'),
                     new InputOption('external-users', null, InputOption::VALUE_REQUIRED, 'External Users start,end revisions'),
                     new InputOption('external-api', null, InputOption::VALUE_REQUIRED, 'External API start,end revisions'),
+                    new InputOption('exclude-self-service', null, InputOption::VALUE_NONE, 'Exclude SelfService repository.'),
+                    new InputOption('exclude-acc4billing', null, InputOption::VALUE_NONE, 'Exclude Acc4billing repository.'),
+                    new InputOption('exclude-dwp', null, InputOption::VALUE_NONE, 'Exclude DWP repository.'),
+                    new InputOption('exclude-external-users', null, InputOption::VALUE_NONE, 'Exclude External Users repository.'),
+                    new InputOption('exclude-external-api', null, InputOption::VALUE_NONE, 'Exclude External API repository.'),
                 ])
             );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->input = $input;
         $this->command = $this->getApplication()->find('svn:log');
         $svnLogOutput = new BufferedOutput();
 
         // self-service
-        if ($input->getOption('self-service'))
+        if (!$input->getOption('exclude-self-service'))
         {
-            $svnLogOutput->writeln('<b style="font-size: 14px">SelfService:</b>');
+            $svnLogOutput->writeln($this->__title('SelfService:'));
 
             try {
                 $this->svnLog(
@@ -61,14 +72,14 @@ class Daisy extends Command
 
                 $svnLogOutput->writeln('');
             } catch (ProcessFailedException $e) {
-                $svnLogOutput->writeln('No updates.');
+                $svnLogOutput->writeln("Couldn't fetch updates.");
             }
         }
 
         //acc4billing
-        if ($input->getOption('acc4billing'))
+        if (!$input->getOption('exclude-acc4billing'))
         {
-            $svnLogOutput->writeln('<b style="font-size: 14px">Acc4Billing:</b>');
+            $svnLogOutput->writeln($this->__title('Acc4Billing:'));
 
             try {
                 $this->svnLog(
@@ -81,14 +92,14 @@ class Daisy extends Command
 
                 $svnLogOutput->writeln('');
             } catch (ProcessFailedException $e) {
-                $svnLogOutput->writeln('No updates.');
+                $svnLogOutput->writeln("Couldn't fetch updates.");
             }
         }
 
         // dwp
-        if ($input->getOption('dwp'))
+        if (!$input->getOption('exclude-dwp'))
         {
-            $svnLogOutput->writeln('<b style="font-size: 14px">DWP:</b>');
+            $svnLogOutput->writeln($this->__title('DWP:'));
             
             try {
                 $this->svnLog(
@@ -101,14 +112,14 @@ class Daisy extends Command
 
                 $svnLogOutput->writeln('');
             } catch (ProcessFailedException $e) {
-                $svnLogOutput->writeln('No updates.');
+                $svnLogOutput->writeln("Couldn't fetch updates.");
             }
         }
 
         // external-users
-        if ($input->getOption('external-users'))
+        if (!$input->getOption('exclude-external-users'))
         {
-            $svnLogOutput->writeln('<b style="font-size: 14px">External Users:</b>');
+            $svnLogOutput->writeln($this->__title('External Users:'));
 
             try {
                 $this->svnLog(
@@ -121,14 +132,14 @@ class Daisy extends Command
 
                 $svnLogOutput->writeln('');
             } catch (ProcessFailedException $e) {
-                $svnLogOutput->writeln('No updates.');
+                $svnLogOutput->writeln("Couldn't fetch updates.");
             }
         }
 
         // external-api
-        if ($input->getOption('external-api'))
+        if (!$input->getOption('exclude-external-api'))
         {
-            $svnLogOutput->writeln('<b style="font-size: 14px">External API APP:</b>');
+            $svnLogOutput->writeln($this->__title('External API APP:'));
 
             try {
                 $this->svnLog(
@@ -138,16 +149,18 @@ class Daisy extends Command
                     getenv('EXTERNAL_API_REPOSITORY'),
                     getenv('EXTERNAL_API_REVISION_URL')
                 );
-
-                $svnLogOutput->writeln('');
             } catch (ProcessFailedException $e) {
-                $svnLogOutput->writeln('No updates.');
+                $svnLogOutput->writeln("Couldn't fetch updates.");
             }
         }
 
         $notes = $svnLogOutput->fetch();
 
-        $this->sendMail(explode(',', $input->getOption('notify')), $notes);
+        // Check if we have to send the email
+        if ($input->getOption('send-email'))
+        {
+            $this->sendMail(explode(',', $input->getOption('notify')), $notes);
+        }
 
         $output->writeln($notes);
     }
@@ -161,6 +174,7 @@ class Daisy extends Command
             '--end'             => $this->__end($revisions),
             '--repository'      => $repository,
             '--revision-url'    => $revisionUrl,
+            '--html'            => $input->getOption('html'),
         );
 
         $returnCode = $this->command->run(new ArrayInput($arguments), $output);
@@ -195,14 +209,57 @@ class Daisy extends Command
     private function __start($revisions)
     {
         $parts = explode(',', $revisions);
+        $start = $parts[0];
 
-        return $parts[0];
+        try {
+            if (!$start) {
+                $today = Carbon::today();
+
+                if ($today->dayOfWeek == 1)
+                {
+                    $start = 'last thursday';
+                }
+                elseif ($today->dayOfWeek == 3)
+                {
+                    $start = 'last tuesday';
+                }
+            }
+
+            $date = new Carbon($start);
+            $start = sprintf('{%s}', $date->toDateString());
+        }
+        catch (\Exception $e)
+        {
+            # do nothing
+        }
+
+        return $start;
     }
 
     private function __end($revisions)
     {
         $parts = explode(',', $revisions);
+        $end = isset($parts[1]) ? $parts[1] : 'HEAD';
 
-        return isset($parts[1]) ? $parts[1] : 'HEAD';
+        try {
+            $date = new Carbon($end);
+            $end = sprintf('{%s}', $date->toDateString());
+        }
+        catch (\Exception $e)
+        {
+            # do nothing
+        }
+
+        return $end;
+    }
+
+    private function __title($title)
+    {
+        if ($this->input->getOption('html'))
+        {
+            $title = sprintf('<b style="font-size: 14px">%s:</b>', $title);
+        }
+
+        return $title;
     }
 }
