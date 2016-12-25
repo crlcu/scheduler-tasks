137f1e9e48129f4cda020ab3d046782c9b6af728
Adrian Crisan <crlcu@Crisans-MacBook-Pro.local>
2016-12-25 17:56:37 +0200

    isitup command

---
+++ app/Console/Command/IsItUp.php
@@ -0,0 +1,51 @@
+<?php
+namespace App\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
+use Exception;
+use GuzzleHttp\Client as Http;
+use GuzzleHttp\Exception\RequestException;
+
+class IsItUp extends Command
+{
+    private $http;
+
+    protected function configure()
+    {
+        $this->setName('isitup')
+            ->setDescription("Checks if url is up.")
+            ->setDefinition(
+                new InputDefinition([
+                    new InputOption('url', null, InputOption::VALUE_REQUIRED, 'URL'),
+                    new InputOption('debug', null, InputOption::VALUE_NONE, 'Enable debug output'),
+                ])
+            );
+        
+        $this->http = new Http();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        try {
+            $response = $this->http->request('GET', $input->getOption('url'), [
+                'debug' => $input->getOption('debug')
+            ]);
+
+            if ($input->getOption('debug')) {
+                $output->writeln($response->getBody()->getContents());
+            } else {
+                $output->writeln('Status: ' . $response->getStatusCode());
+            }
+        } catch (RequestException $e) {
+            throw new Exception('Is it down');
+        }
+    }
+}
